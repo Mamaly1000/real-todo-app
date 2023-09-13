aecfad3f5b77e85cf454845f6374e1b606c94f02
Mamaly1000 <105161078+Mamaly1000@users.noreply.github.com>
2023-09-12 17:39:40 -0700

    reset next-auth configuration

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,14 +2,12 @@ import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import clientPromise from "./../../../server/lib/mongoDB";
-export const authOptions = {
-  // Configure one or more authentication providers
+export default NextAuth({
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
-    // ...add more providers here
   ],
   adapter: MongoDBAdapter(clientPromise),
   session: {
@@ -19,18 +17,19 @@ export const authOptions = {
     secret: process.env.SECRET_JWT,
   },
   callbacks: {
-    async jwt({ token, account }) {
-      if (account) {
-        token.accessToken = account.access_token;
-        token.id = profile.id;
-      }
-      return token;
-    },
-    async session({ session, token }) {
+    async session({ session, token, user }) {
       session.user.id = token.id;
       session.accessToken = token.accessToken;
       return session;
     },
+    async jwt({ token, user, account, profile, isNewUser }) {
+      if (user) {
+        token.id = user.id;
+      }
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
   },
-};
-export default NextAuth(authOptions);
+});
